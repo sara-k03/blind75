531295b53daf0ec1337b5cd9578d79df3c5dec9e
sara-k03 <sarayu.kondaveeti@gmail.com>
2025-06-09 14:57:30 -0400

    Finished TwoSum

--- TwoSum/Solution.py
+++ TwoSum/Solution.py
@@ -6,4 +6,14 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        
+        # Initial solution: Runtime only beats 5% of solutions on LeetCode
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)):
+        #         if (not i == j) and (nums[i] + nums[j] == target):
+        #             return [i, j]
+
+        # Another solution
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
---
+++ TwoSum/main.py
@@ -0,0 +1,6 @@
+from Solution import * 
+
+nums = [2,7,11,15]
+target = 9
+
+print(Solution.twoSum(Solution, nums, target))
